9ab4a91775dea7056b7883ef5e1130f25149c6af
Kuba <kubacki.jk@gmail.com>
2012-03-01 19:17:38 +0100

    Pierwszy commit

---
+++ src/chess/Chess.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+/**
+ *
+ * @author Pawel
+ */
+public class Chess {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
