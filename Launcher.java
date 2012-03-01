334e57c4c0b19d0a136d554bbe2139ba8df556ba
Kuba <kubacki.jk@gmail.com>
2012-03-01 20:38:28 +0100

    Launcher Uruchamiający Board

---
+++ Launcher.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+
+public class Launcher {
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		board.setVisible(true);
+		
+	}
+}
